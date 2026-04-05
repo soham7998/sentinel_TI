2986472ecc8840d9607d3a8b791e35b26ae4e238
Soham Shah <soham27@somaiya.edu>
2026-04-05 09:49:43 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ import numpy as np
 import streamlit as st
 from datetime import datetime, timezone, timedelta
 
-BACKEND = os.getenv("BACKEND_URL", "http://backend:8000")
+BACKEND = os.getenv("https://sentinelti-production.up.railway.app")
 
 st.set_page_config(page_title="SentinelTI | SOC", page_icon="🛡️",
                    layout="wide", initial_sidebar_state="expanded")
